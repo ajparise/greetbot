3f56e476c0573d42fe58ebb8e15c5e34521190cb
Angelo Cordon <angelocordon@gmail.com>
2018-02-18 19:29:41 -0800

    POST spec

--- test/src/index.js
+++ test/src/index.js
@@ -11,4 +11,41 @@ describe('API Routes', function () {
         });
     });
   });
+
+  describe('POST /welcome', function () {
+    describe('when calling /welcome foo', function () {
+      it('should call the foo test', function (done) {
+        const request = {
+          body: {
+            token: 'gK3YDdCtddql7fFOka0uSq1a',
+            team_id: 'T8ZSYHBD2',
+            team_domain: 'violins',
+            channel_id: 'D93KK60FN',
+            channel_name: 'directmessage',
+            user_id: 'U930WKW8G',
+            user_name: 'angelocordon',
+            command: '/welcome',
+            text: 'test',
+            response_url: 'https://hooks.slack.com/commands/T8ZSYHBD2/316329130113/l41UZnEABvaGSRGzRBsse8z5',
+            trigger_id: '316329130177.305916589444.0d141841d6d1599adf447182d93ae2f2'
+          }
+        };
+
+        request.post('/welcome')
+          .send(request)
+          .end(function (err, res) {
+            console.log(res);
+            done(err);
+          })
+      });
+    });
+
+    describe('when calling /welcome python', function () {
+      it('should return the python message');
+    });
+
+    describe('when calling /welcome test', function () {
+      it('should return with the welcome message');
+    });
+  });
 });
